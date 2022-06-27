5ac9ffd14cc77330d88d1f7fb87e6addca7968ce
shelly <shelly.gk@gmail.com>
2022-06-27 18:08:27 +0300

    adding the project to github

---
+++ jScripts/contactUs.js
@@ -0,0 +1,10 @@
+﻿function openPopUP() {
+    var lname = document.getElementById('lname').value;
+    var fname = document.getElementById('fname').value;
+    var alertd = document.getElementsByClassName('alert');
+
+
+    if (lname != "" && fname != "") {
+        alert("ההודעה נשלחה בהצלחה");
+    }
+}
